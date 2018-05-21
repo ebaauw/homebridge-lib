e3493954b019b1d2c81b651c745ed4b95dd5d43a
ebaauw <erik.baauw@xs4all.nl>
2018-05-21 11:37:12 +0200

    Major refactor
    Move generic files from homebridge-hue-utils to homebridge-lib.

--- index.js
+++ index.js
@@ -8,9 +8,17 @@
 'use strict'
 
 module.exports = {
+  CustomHomeKitTypes: require('./lib/CustomHomeKitTypes'),
+  EveHomeKitTypes: require('./lib/EveHomeKitTypes'),
   LibPlatform: require('./lib/LibPlatform'),
   LibAccessory: require('./lib/LibAccessory'),
   LibService: require('./lib/LibService'),
   LibCharacteristic: require('./lib/LibCharacteristic'),
-  LibObject: require('./lib/LibObject')
+  LibObject: require('./lib/LibObject'),
+  JsonFormatter: require('./lib/JsonFormatter'),
+  MyHomeKitTypes: require('./lib/MyHomeKitTypes'),
+  OptionParser: require('./lib/OptionParser'),
+  RestClient: require('./lib/RestClient'),
+  UpnpMonitor: require('./lib/UpnpMonitor'),
+  WsMonitor: require('./lib/WsMonitor')
 }
