3745acf2ee955216abc406762285173c661e6d88
ebaauw <erik.baauw@xs4all.nl>
2020-01-02 14:40:03 +0100

    No longer expose command-line tools as modules.
    With latest version of `npm` install will fail if `bin` symlinks already exist.

--- cli/json.js
+++ cli/json.js
@@ -11,4 +11,117 @@
 
 const homebridgeLib = require('../index')
 
-new homebridgeLib.JsonCommand().main()
+const chalk = require('chalk')
+const fs = require('fs')
+
+const b = chalk.bold
+const u = chalk.underline
+const usage = `${b('json')} [${b('-hVsnjuatlkv')}] [${b('-p')} path] [${b('-d')} depth] [${b('-c')} ${u('string')}]... [${u('file')}]...`
+const help = `JSON formatter.
+
+Usage: ${usage}
+
+By default, ${b('json')} reads JSON from stdin, formats it, and prints it to stdout.
+The following parameters modify this behaviour:
+  ${b('-h')}          Print this help and exit.
+  ${b('-V')}          Print version and exit.
+  ${b('-s')}          Sort object key/value pairs alphabetically on key.
+  ${b('-n')}          Do not include spaces nor newlines in output.
+  ${b('-j')}          Output JSON array of objects for each key/value pair.
+              Each object contains two key/value pairs: key "keys" with an array
+              of keys as value and key "value" with the value as value.
+  ${b('-u')}          Output JSON array of objects for each key/value pair.
+              Each object contains one key/value pair: the path (concatenated
+              keys separated by '/') as key and the value as value.
+  ${b('-a')}          Output path:value in plain text instead of JSON.
+  ${b('-t')}          Limit output to top-level key/values.
+  ${b('-p')} ${u('path')}     Limit output to key/values under ${u('path')}. Set top level below ${u('path')}.
+  ${b('-d')} ${u('depth')}    Limit output to levels above ${u('depth')}.
+  ${b('-l')}          Limit output to leaf (non-array, non-object) key/values.
+  ${b('-k')}          Limit output to keys. With ${b('-u')} output JSON array of paths.
+  ${b('-v')}          Limit output to values. With ${b('-u')} output JSON array of values.
+  ${b('-c')} ${u('string')}   Read JSON from ${u('string')} instead of from stdin.
+  ${u('file')}        Read JSON from ${u('file')} instead of from stdin.`
+
+class Main extends homebridgeLib.CommandLineTool {
+  constructor () {
+    super()
+    this.usage = usage
+    this.options = {}
+    this.stringList = []
+    this.fileList = []
+  }
+
+  parseArguments () {
+    const parser = new homebridgeLib.CommandLineParser()
+    parser.help('h', 'help', help)
+    parser.version('V', 'version')
+    parser.flag('s', 'sortKeys', () => { this.options.sortKeys = true })
+    parser.flag('n', 'noWhiteSpace', () => { this.options.noWhiteSpace = true })
+    parser.flag('j', 'jsonArray', () => { this.options.jsonArray = true })
+    parser.flag('u', 'joinKeys', () => { this.options.joinKeys = true })
+    parser.flag('a', 'ascii', () => { this.options.ascii = true })
+    parser.flag('t', 'topOnly', () => { this.options.topOnly = true })
+    parser.option('d', 'maxDepth', (value, option) => {
+      this.options.maxDepth = homebridgeLib.OptionParser.toInt(
+        'maxDepth', value, 0, null, true)
+    })
+    parser.option('p', 'fromPath', (value, option) => {
+      this.options.fromPath = homebridgeLib.OptionParser.toPath(
+        'fromPath', value, true, true
+      )
+    })
+    parser.flag('l', 'leavesOnly', () => { this.options.leavesOnly = true })
+    parser.flag('k', 'keysOnly', () => { this.options.keysOnly = true })
+    parser.flag('v', 'valuesOnly', () => { this.options.valuesOnly = true })
+    parser.option('c', 'string', (value) => { this.stringList.push(value) })
+    parser.remaining((list) => { this.fileList = list })
+    parser.parse()
+  }
+
+  processString (s) {
+    let value
+    try {
+      value = JSON.parse(s)
+    } catch (err) {
+      throw new Error(err.message)
+    }
+    const output = this.jsonFormatter.stringify(value)
+    if (this.n++ > 0) {
+      this.print('------')
+    }
+    if (output !== '') {
+      this.print(output)
+    }
+  }
+
+  main () {
+    try {
+      this.parseArguments()
+      this.jsonFormatter = new homebridgeLib.JsonFormatter(this.options)
+      if (this.fileList.length === 0 && this.stringList.length === 0) {
+        this.fileList = ['-']
+      }
+      this.n = 0
+      this.stringList.map((s) => {
+        try {
+          this.processString(s)
+        } catch (err) {
+          this.error(err)
+        }
+      })
+      this.fileList.map((file) => {
+        try {
+          const s = fs.readFileSync(file === '-' ? 0 : file, 'utf8')
+          this.processString(s)
+        } catch (err) {
+          this.error(err)
+        }
+      })
+    } catch (err) {
+      this.fatal(err)
+    }
+  }
+}
+
+new Main().main()
--- cli/upnp.js
+++ cli/upnp.js
@@ -11,4 +11,120 @@
 
 const homebridgeLib = require('../index')
 
-new homebridgeLib.UpnpCommand().main()
+const chalk = require('chalk')
+
+const b = chalk.bold
+const u = chalk.underline
+const usage = `${b('upnp')} [${b('-hVadnrsz')}] [${b('-c')} ${u('class')}] [${b('-t')} ${u('timeout')}]`
+const help = `UPnP tool.
+
+Search for UPnP devices and print found devices as JSON.
+When running as daemon or service, log UPnP alive broadcasts as JSON.
+
+Usage: ${usage}
+
+Parameters:
+  ${b('-h')}, ${b('--help')}
+  Print this help and exit.
+
+  ${b('-V')}, ${b('--version')}
+  Print version and exit.
+
+  ${b('-a')}, ${b('--all')}
+  Short for ${b('-c ssdp:all')}.
+
+  ${b('-c')} ${u('class')}, ${b('--class=')}${u('class')}
+  Search for ${u('class')} instead of default ${b('ssdp:rootdevice')}.
+
+  ${b('-d')}, ${b('--daemon')}
+  Run as daemon.  Listen for UPnP alive broadcasts instead of searching.
+
+  ${b('-n')}, ${b('--noWhiteSpace')}
+  Do not include spaces nor newlines in JSON output.
+
+  ${b('-r')}, ${b('--raw')}
+  Do not parse messages, output raw message data instead of JSON.
+
+  ${b('-s')}, ${b('--service')}
+  Run as daemon.  Listen for UPnP alive broadcasts instead of searching.
+
+  ${b('-t')} ${u('timeout')}, ${b('--timeout=')}${u('timeout')}
+  Search for ${u('timeout')} seconds instead of default ${b('5')}.
+
+  ${b('-z')}, ${b('--ZonePlayer')}
+  Short for ${b('-c urn:schemas-upnp-org:device:ZonePlayer:1')}.`
+
+class Main extends homebridgeLib.CommandLineTool {
+  constructor () {
+    super()
+    this.usage = usage
+    this.options = {
+      noWhiteSpace: false
+    }
+    this.upnp = {}
+  }
+
+  parseArguments () {
+    const parser = new homebridgeLib.CommandLineParser()
+    parser.help('h', 'help', help)
+    parser.version('V', 'version')
+    parser.flag('a', 'all', (key) => { this.upnp.class = 'ssdp:all' })
+    parser.option('c', 'class', (value, key) => { this.upnp.class = value })
+    parser.flag('d', 'daemon', (key) => { this.options.mode = 'daemon' })
+    parser.flag('n', 'noWhiteSpace', () => { this.options.noWhiteSpace = true })
+    parser.flag('r', 'raw', (key) => { this.options.raw = true })
+    parser.flag('s', 'service', (key) => { this.options.mode = 'service' })
+    parser.option('t', 'timeout', (value, key) => {
+      this.upnp.timeout = homebridgeLib.OptionParser.toInt(
+        'timeout', value, 1, 60, true
+      )
+    })
+    parser.flag('z', 'ZonePlayer', (key) => {
+      this.upnp.class = 'urn:schemas-upnp-org:device:ZonePlayer:1'
+    })
+    parser.parse()
+  }
+
+  exit (signal) {
+    this.log('got %s - exiting', signal)
+    process.exit(0)
+  }
+
+  main () {
+    try {
+      this.parseArguments()
+      const jsonFormatter = new homebridgeLib.JsonFormatter({
+        noWhiteSpace: this.options.noWhiteSpace
+      })
+      const unpnClient = new homebridgeLib.UpnpClient(this.upnp)
+      process.on('SIGINT', () => { this.exit('SIGINT') })
+      process.on('SIGTERM', () => { this.exit('SIGTERM') })
+      unpnClient.on('listening', (host) => { this.log('listening on %s', host) })
+      unpnClient.on('deviceAlive', (address, obj, message) => {
+        if (!this.options.raw) {
+          message = jsonFormatter.stringify(obj)
+        }
+        this.log('%s: %s', address, message)
+      })
+      unpnClient.on('searching', (host) => { this.log('searching on %s', host) })
+      unpnClient.on('searchDone', () => { this.log('search done') })
+      unpnClient.on('deviceFound', (address, obj, message) => {
+        if (!this.options.raw) {
+          message = jsonFormatter.stringify(obj)
+        }
+        this.print('%s: %s', address, message)
+      })
+      unpnClient.on('error', (err) => { this.error(err) })
+      if (this.options.mode) {
+        this.setOptions({ mode: this.options.mode })
+        unpnClient.listen()
+      } else {
+        unpnClient.search()
+      }
+    } catch (err) {
+      this.fatal(err)
+    }
+  }
+}
+
+new Main().main()
