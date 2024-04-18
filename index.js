a6f242ef1419ec81f8fdfebf4dfcd18beb22da1d
ebaauw <erik.baauw@xs4all.nl>
2024-04-18 17:48:53 +0200

    Update index.js
    - Convert to ECMAScript model;
    - `UIServer`: remove for now, as it causes a SIGTERM of Homebridge, ~9s after start.

--- index.js
+++ index.js
@@ -3,10 +3,6 @@
 // Library for Homebridge plugins.
 // Copyright © 2017-2024 Erik Baauw. All rights reserved.
 
-'use strict'
-
-const hbLibTools = require('hb-lib-tools')
-
 /** Library for Homebridge plugins.
   * see the {@tutorial homebridgeLib} tutorial.
   *
@@ -38,7 +34,7 @@ const hbLibTools = require('hb-lib-tools')
   * To access the classes provided by Homebridge Lib from your module,
   * simply load it by:
   * ```javascript
-  * const homebridgeLib = require('homebridge-lib')
+  * import homebridgeLib from 'homebridge-lib'
   * ```
   *
   * Note that each class provided by Homebridge Lib is implemented as a
@@ -48,205 +44,228 @@ const hbLibTools = require('hb-lib-tools')
   *
   * @module homebridgeLib
   */
-class homebridgeLib {
-  /** Delegate of a HomeKit accessory.
-    * <br>See {@link AccessoryDelegate}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get AccessoryDelegate () { return require('./lib/AccessoryDelegate') }
 
-  /** Adaptive Lighting.
-    * <br>See {@link AdaptiveLighting}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get AdaptiveLighting () { return require('./lib/AdaptiveLighting') }
+/** Abstract superclass for {@link Platform}, {@link AccessoryDelegate},
+  * {@link ServiceDelegate}, and {@link CharacteristicDelegate}.
+  * <br>See {@link Delegate}.
+  * @name Delegate
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { Delegate } from './lib/Delegate.js'
 
-  /** Return the `Bonjour` class from [`bonjour-hap`](https://github.com/homebridge/bonjour),
-    * so plugins don't have to list this as a separate dependency.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get Bonjour () { return hbLibTools.Bonjour }
+/** Delegate of a HomeKit accessory.
+  * <br>See {@link AccessoryDelegate}.
+  * @name AccessoryDelegate
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { AccessoryDelegate } from './lib/AccessoryDelegate.js'
 
-  /** Delegate of a HomeKit characteristic.
-    * <br>See {@link CharacteristicDelegate}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get CharacteristicDelegate () { return require('./lib/CharacteristicDelegate') }
+/** Adaptive Lighting.
+  * <br>See {@link AdaptiveLighting}.
+  * @name AdaptiveLighting
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { AdaptiveLighting } from './lib/AdaptiveLighting.js'
 
-  /** Colour conversions.
-    * <br>See {@link Colour}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get Colour () { return hbLibTools.Colour }
+/** Return the `Bonjour` class from [`bonjour-hap`](https://github.com/homebridge/bonjour),
+  * so plugins don't have to list this as a separate dependency.
+  * @name Bonjour
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { Bonjour } from 'hb-lib-tools'
 
-  /** Parser and validator for command-line arguments.
-    * <br>See {@link CommandLineParser}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get CommandLineParser () { return hbLibTools.CommandLineParser }
+/** Delegate of a HomeKit characteristic.
+  * <br>See {@link CharacteristicDelegate}.
+  * @name CharacteristicDelegate
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { CharacteristicDelegate } from './lib/CharacteristicDelegate.js'
 
-  /** Command-line tool.
-    * <br>See {@link CommandLineTool}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get CommandLineTool () { return hbLibTools.CommandLineTool }
+/** Colour conversions.
+  * <br>See {@link Colour}.
+  * @name Colour
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { Colour } from 'hb-lib-tools'
 
-  /** Abstract superclass for {@link EveHomeKitTypes}
-    * and {@link MyHomeKitTypes}.
-    * <br>See {@link CustomHomeKitTypes}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get CustomHomeKitTypes () { return require('./lib/CustomHomeKitTypes') }
+/** Parser and validator for command-line arguments.
+  * <br>See {@link CommandLineParser}.
+  * @name CommandLineParser
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { CommandLineParser } from 'hb-lib-tools'
 
-  /** Abstract superclass for {@link Platform}, {@link AccessoryDelegate},
-    * {@link ServiceDelegate}, and {@link CharacteristicDelegate}.
-    * <br>See {@link Delegate}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get Delegate () { return require('./lib/Delegate') }
+/** Command-line tool.
+  * <br>See {@link CommandLineTool}.
+  * @name CommandLineTool
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { CommandLineTool } from 'hb-lib-tools'
 
-  /** Custom HomeKit services and characteristics used by
-    * [Eve](https://www.evehome.com/en) accessories and by the
-    * [Eve app](https://www.evehome.com/en/eve-app).
-    * <br>See {@link EveHomeKitTypes}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get EveHomeKitTypes () { return require('./lib/EveHomeKitTypes') }
+/** Abstract superclass for {@link EveHomeKitTypes}
+  * and {@link MyHomeKitTypes}.
+  * <br>See {@link CustomHomeKitTypes}.
+  * @name CustomHomeKitTypes
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { CustomHomeKitTypes } from './lib/CustomHomeKitTypes.js'
 
-  /** HTTP client.
-    * <br>See {@link HttpClient}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get HttpClient () { return hbLibTools.HttpClient }
+/** Custom HomeKit services and characteristics used by
+  * [Eve](https://www.evehome.com/en) accessories and by the
+  * [Eve app](https://www.evehome.com/en/eve-app).
+  * <br>See {@link EveHomeKitTypes}.
+  * @name EveHomeKitTypes
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { EveHomeKitTypes } from './lib/EveHomeKitTypes.js'
 
-  /** JSON formatter.
-    * <br>See {@link JsonFormatter}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get JsonFormatter () { return hbLibTools.JsonFormatter }
+/** HTTP client.
+  * <br>See {@link HttpClient}.
+  * @name HttpClient
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { HttpClient } from 'hb-lib-tools'
 
-  /** My own collection of custom HomeKit services and characteristics.
-    * <br>See {@link MyHomeKitTypes}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get MyHomeKitTypes () { return require('./lib/MyHomeKitTypes') }
+/** JSON formatter.
+  * <br>See {@link JsonFormatter}.
+  * @name JsonFormatter
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { JsonFormatter } from 'hb-lib-tools'
 
-  /** Parser and validator for options and other parameters.
-    * <br>See {@link OptionParser}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get OptionParser () { return hbLibTools.OptionParser }
+/** My own collection of custom HomeKit services and characteristics.
+  * <br>See {@link MyHomeKitTypes}.
+  * @name MyHomeKitTypes
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { MyHomeKitTypes } from './lib/MyHomeKitTypes.js'
 
-  /** Homebridge dynamic platform plugin.
-    * <br>See {@link Platform}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get Platform () { return require('./lib/Platform') }
+/** Parser and validator for options and other parameters.
+  * <br>See {@link OptionParser}.
+  * @name OptionParser
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { OptionParser } from 'hb-lib-tools'
 
-  /** Delegate of a property of a HomeKit accessory or service.
-    * <br>See {@link PropertyDelegate}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get PropertyDelegate () { return require('./lib/PropertyDelegate') }
+/** Homebridge dynamic platform plugin.
+  * <br>See {@link Platform}.
+  * @name Platform
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { Platform } from './lib/Platform.js'
 
-  /** Delegate of a HomeKit service.
-    * <br>See {@link ServiceDelegate}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get ServiceDelegate () { return require('./lib/ServiceDelegate') }
+/** Delegate of a property of a HomeKit accessory or service.
+  * <br>See {@link PropertyDelegate}.
+  * @name PropertyDelegate
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { PropertyDelegate } from './lib/PropertyDelegate.js'
 
-  /** System information.
-    * <br>See {@link SystemInfo}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get SystemInfo () { return hbLibTools.SystemInfo }
+/** Delegate of a HomeKit service.
+  * <br>See {@link ServiceDelegate}.
+  * @name ServiceDelegate
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { ServiceDelegate } from './lib/ServiceDelegate/index.js'
 
-  /** Universal Plug and Play client.
-    * <br>See {@link UpnpClient}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get UpnpClient () { return hbLibTools.UpnpClient }
+/** System information.
+  * <br>See {@link SystemInfo}.
+  * @name SystemInfo
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { SystemInfo } from 'hb-lib-tools'
 
-  /** Server for dynamic configuration settings through Homebridge UI.
-    * <br>See {@link UiServer}.
-    * @type {Class}
-    * @memberof module:homebridgeLib
-    */
-  static get UiServer () { return require('./lib/UiServer') }
+/** Universal Plug and Play client.
+  * <br>See {@link UpnpClient}.
+  * @name UpnpClient
+  * @type {Class}
+  * @memberof module:homebridgeLib
+  */
+export { UpnpClient } from 'hb-lib-tools'
 
-  /** Resolve after given period, delaying execution.
-    *
-    * E.g. to delay execution for 1.5 seconds, issue:
-    * ```javascript
-    *   await homebridgeLib.timeout(1500)
-    * ```
-    *
-    * @param {integer} msec - Period (in msec) to wait.
-    * @throws {TypeError} On invalid parameter type.
-    * @throws {RangeError} On invalid parameter value.
-    * @memberof module:homebridgeLib
-    */
-  static get timeout () { return hbLibTools.timeout }
+// /** Server for dynamic configuration settings through Homebridge UI.
+//   * <br>See {@link UiServer}.
+//   * @name UiServer
+//   * @type {Class}
+//   * @memberof module:homebridgeLib
+//   */
+// export { UiServer } from './lib/UiServer.js'
 
-  /** Convert Error to string.
-    *
-    * Include the stack trace only for programming errors (JavaScript and NodeJS
-    * runtime errors).
-    * Translate system errors into more readable messages.
-    * @param {Error} e - The error.
-    * @param {boolean} [useChalk=false] - Use chalk to grey out the stack trace.
-    * @returns {string} - The error as string.
-    * @memberof module:homebridgeLib
-    */
-  static get formatError () { return hbLibTools.formatError }
+/** Convert Error to string.
+  *
+  * Include the stack trace only for programming errors (JavaScript and NodeJS
+  * runtime errors).
+  * Translate system errors into more readable messages.
+  * @param {Error} e - The error.
+  * @param {boolean} [useChalk=false] - Use chalk to grey out the stack trace.
+  * @returns {string} - The error as string.
+  * @memberof module:homebridgeLib
+  */
+export { formatError } from 'hb-lib-tools'
 
-  /** Convert integer to hex string.
-    * @param {integer} i - The integer.
-    * @param {integer} [length=4] - The number of digits in the hex string.
-    * @returns {string} - The hex string.
-    * @memberof module:homebridgeLib
-    */
-  static get toHexString () { return hbLibTools.toHexString }
+/** Return the recommended version of NodeJS from package.json.
+  * This is the version used to develop and test the software,
+  * typically the latest LTS version.
+  * @param {string} packageJson - The contents of package.json
+  * #return {string} - The recommended version.
+  * @memberof module:hbLibTools
+  */
+export { recommendedNodeVersion } from 'hb-lib-tools'
 
-  /** Return the [`chalk`](https://github.com/chalk/chalk) module,
-    * so plugins don't have to list this as a separate dependency.
-    * @memberof module:homebridgeLib
-    */
-  static get chalk () { return hbLibTools.chalk }
+/** Resolve after given period, delaying execution.
+  *
+  * E.g. to delay execution for 1.5 seconds, issue:
+  * ```javascript
+  *   await homebridgeLib.timeout(1500)
+  * ```
+  *
+  * @param {integer} msec - Period (in msec) to wait.
+  * @throws {TypeError} On invalid parameter type.
+  * @throws {RangeError} On invalid parameter value.
+  * @memberof module:homebridgeLib
+  */
+export { timeout } from 'hb-lib-tools'
 
-  /** Return the [`semver`](https://github.com/npm/node-semver) module,
-    * so plugins don't have to list this as a separate dependency.
-    * @memberof module:homebridgeLib
-    */
-  static get semver () { return hbLibTools.semver }
+/** Convert integer to hex string.
+  * @param {integer} i - The integer.
+  * @param {integer} [length=4] - The number of digits in the hex string.
+  * @returns {string} - The hex string.
+  * @memberof module:homebridgeLib
+  */
+export { toHexString } from 'hb-lib-tools'
 
-  /** Return the recommended version of NodeJS from package.json.
-    * This is the version used to develop and test the software,
-    * typically the latest LTS version.
-    * @param {string} packageJson - The contents of package.json
-    * #return {string} - The recommended version.
-    * @memberof module:hbLibTools
-    */
-  static get recommendedNodeVersion () { return hbLibTools.recommendedNodeVersion }
-}
+/** Return the [`chalk`](https://github.com/chalk/chalk) module,
+  * so plugins don't have to list this as a separate dependency.
+  * @memberof module:homebridgeLib
+  */
+export { chalk } from 'hb-lib-tools'
 
-module.exports = homebridgeLib
+/** Return the [`semver`](https://github.com/npm/node-semver) module,
+  * so plugins don't have to list this as a separate dependency.
+  * @memberof module:homebridgeLib
+  */
+export { semver } from 'hb-lib-tools'
+
+export { AccessoryInformation } from './lib/ServiceDelegate/AccessoryInformation.js'
+export { Battery } from './lib/ServiceDelegate/Battery.js'
+export { Dummy } from './lib/ServiceDelegate/Dummy.js'
+export { History } from './lib/ServiceDelegate/History.js'
+export { ServiceLabel } from './lib/ServiceDelegate/ServiceLabel.js'
